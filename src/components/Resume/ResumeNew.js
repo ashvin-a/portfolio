c5df5204ca094412c548e5bf96010b99c6f0a529
ashvin <ashvinanilkumarsh@gmail.com>
2026-01-08 15:34:00 -0800

    Resolve issue with displaying Resume.

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -6,6 +6,7 @@ import pdf from "../../Assets/../Assets/Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css"; 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 function ResumeNew() {
@@ -33,8 +34,24 @@ function ResumeNew() {
 
         <Row className="resume">
           <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+            <Page
+              pageNumber={1}
+              scale={width > 786 ? 1.7 : 0.6}
+              renderTextLayer={false} // <--- Add this prop
+              renderAnnotationLayer={false} // Optional: removes links if you don't need them
+            />
           </Document>
+        </Row>
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={pdf}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
         </Row>
       </Container>
     </div>
